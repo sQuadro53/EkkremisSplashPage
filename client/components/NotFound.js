fddd4910387a58d6cf3d1340f62e43f921d26f14
Cesar-Quintanilla <quintanilla.cesar@ymail.com>
2022-11-07 14:55:17 -0800

    working on client side and setting up run scripts

---
+++ client/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+
+    return (
+        <div>Not Found</div>
+    )
+
+}
+
+
+export default NotFound;
--- server/routes/demoAPI.js
+++ server/routes/demoAPI.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
-const demoController = require('../controllers/demoController');
 const data = require('../public/data.json');
 
 router.get('/', (req, res) => {
