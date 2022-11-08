8f336d9f010f5c2ad32055dd4d351f830529c5d1
forestleigh <theforestleigh@gmail.com>
2022-11-08 14:25:50 -0800

    added began documents page

--- client/components/Docs.js
+++ client/components/Docs.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import TopBar from './TopBar';
+import '../styles/Docs.css';
 
 const Docs = () => {
 
     return (
+      <div>
         <div>
-            <TopBar />
-            <img className='header-logo' src={require('../img/Ekkremis_Banner.jpg')} alt="Ekkremis" height='100px' width='180px'/>
-            <div className="table-of-contents">
-                <img className='header-logo' src={require('../img/Ekkremis-sm.png')} alt="Ekkremis" height='100px' width='180px'/>
-                <a href=""> </a>
-                <a href=""> </a>
-                <a href=""> </a>
-                <a href=""> </a>
-            </div>
-            <div className="docs"> 
-                <h2></h2>
-                <p></p>
-            </div>
+          <TopBar />
         </div>
-    )
+
+        <div className="table-of-contents">
+          <img
+            className="header-logo"
+            src={require("../img/Ekkremis-sm.png")}
+            alt="Ekkremis"
+            height="100px"
+            width="180px"
+          />
+          <a href=""> </a>
+          <a href=""> </a>
+          <a href=""> </a>
+          <a href=""> </a>
+        </div>
+        <div className="docs">
+          <img
+            className="docs-banner"
+            src={require("../img/Ekkremis_Banner.jpg")}
+            alt="Ekkremis Banner"
+            height="100px"
+            width="180px"
+          />
+          <h1>Introducing Ekkremis</h1>
+          <p></p>
+        </div>
+      </div>
+    );
 
 }
 
