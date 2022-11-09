792fe209ded7c81f6f7a75f46441d65824552d39
forestleigh <theforestleigh@gmail.com>
2022-11-09 00:02:36 -0800

    docs are complete

--- client/components/NotFound.js
+++ client/components/NotFound.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import TopBar from './TopBar';
+import React from "react";
+import TopBar from "./TopBar";
+import { useRouteError } from "react-router-dom";
 
 const NotFound = () => {
+  const error = useRouteError();
+  console.error(error);
 
-    return (
-        <TopBar />
-    )
-
-}
+  return (
+    <div id="not-found-container">
+      <TopBar />
+      <img src="../img/Ekkremis-md.png" alt="Page not Found" />
+      <h1>Oops!</h1>
+      <p>
+        Sorry, this part of the ocean is too deep even for Ekkremis. Try
+        searching in a different endpoint.
+      </p>
+    </div>
+  );
+};
 
 export default NotFound;
